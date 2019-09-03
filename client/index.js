091b6749a015d1c50377e70c5d417e717c7482cd
Arkansio <38956983+Arkansio@users.noreply.github.com>
2019-09-03 03:41:41 +0200

    add example

---
+++ client/index.js
@@ -0,0 +1,39 @@
+const grpc = require('grpc')
+
+const PROTO_PATH = '../server/notes.proto'
+const NoteService = grpc.load(PROTO_PATH).NoteService
+const client = new NoteService('localhost:50051', grpc.credentials.createInsecure())
+
+const newNote = {
+    title: "new note",
+    content: "beautiful"
+}
+
+function list() {
+    client.list({}, (error, notes) => {
+        if (!error) {
+            console.log('successfully fetch List notes');
+            console.log(notes);
+        } else {
+            console.log(error);
+        }
+    })
+    
+}
+
+client.insert(newNote, (error, note) => {
+    if (!error) {
+        console.log('New note created successfully', note);
+    } else {
+        console.error(error);
+    }
+})
+
+client.delete({ id: '2' }, (error, note) => {
+    if (!error) {
+        console.log('Successfully deleted');
+        list();
+    } else {
+        console.log(error)
+    }
+})
